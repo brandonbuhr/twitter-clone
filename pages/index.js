3b2c6985b655c2add9742176c08e7ecf4c7f7a5a
brandonbuhr <brandonbuhr35@gmail.com>
2024-09-08 16:12:37 -0500

    added bottom banner

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import Sidebar from "@/components/Sidebar";
 import PostFeed from "@/components/Postfeed";
 import Trending from "@/components/Trending";
+import BottomBanner from "@/components/BottomBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
-      <Sidebar />
-      <PostFeed /> 
-      <Trending />
+    <div>
+      <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
+        <Sidebar />
+        <PostFeed />
+        <Trending />
+      </div>
+      <BottomBanner />
     </div>
   );
 }
