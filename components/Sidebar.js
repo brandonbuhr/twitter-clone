b709b6494d0d83b6f9f58eeb26bb5d3baa02cd27
brandonbuhr <brandonbuhr35@gmail.com>
2024-09-08 21:47:23 -0500

    main structure for sidebar profile icon complete

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,26 +1,39 @@
+import { auth } from "@/firebase";
+import { signOutUser } from "@/redux/userSlice";
 import {
   HomeIcon,
   HashtagIcon,
   InboxIcon,
   BookmarkIcon,
-  ClipboardListIcon,
   BellIcon,
   UserIcon,
   DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import { signOut } from "firebase/auth";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Sidebar() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+
+  async function handleSignOut() {
+    await signOut(auth);
+    dispatch(signOutUser());
+  }
+
   return (
-    <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+    <div className="h-full hidden sm:flex flex-col fixed xl:ml-24 ml-2">
       <nav className="h-full relative xl:space-y-1.5">
-      <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
-        <Image
-          src={"/assets/twitter-brands-solid.svg"}
-          width={34}
-          height={34}
-        />
-      </div>
+        <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
+          <Image
+            src={"/assets/twitter-brands-solid.svg"}
+            width={34}
+            height={34}
+          />
+        </div>
         <SidebarLink Icon={HomeIcon} text={"Home"} />
         <SidebarLink Icon={HashtagIcon} text={"Explore"} />
         <SidebarLink Icon={BellIcon} text={"Notifications"} />
@@ -28,11 +41,28 @@ export default function Sidebar() {
         <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
         <SidebarLink Icon={UserIcon} text={"Profile"} />
         <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
-        <button className="hidden xl:inline bg-[#1d9bf0]
+        <button
+          className="hidden xl:inline bg-[#1d9bf0]
         rounded-full h-[52px] w-[200px] mt-2 text-lg font-bold
-        ">
-            Tweet</button>
-      <div className="absolute bottom-0">User</div>
+        "
+        >
+          Tweet
+        </button>
+        <div
+          onClick={handleSignOut}
+          className="bottom-0 hover:bg-white hover:bg-opacity-10 rounded-full cursor-pointer absolute flex justify-center 
+        items-center xl:p-3 space-x-3"
+        >
+          <img
+            className="w-10 h-10 rounded-full object-cover"
+            src={user.photoUrl || "/assets/kylie.png"}
+          />
+          <div className="hidden xl:inline">
+            <h1 className="font-bold whitespace-nowrap">{user.name}</h1>
+            <h1 className="text-gray-500">@{user.username}</h1>
+          </div>
+          <DotsHorizontalIcon className="h-5 hidden xl:inline" />
+        </div>
       </nav>
     </div>
   );
