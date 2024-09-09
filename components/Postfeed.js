bcf0164e448b1b5182df357c79f8ef099dc3ce51
brandonbuhr <brandonbuhr35@gmail.com>
2024-09-09 13:17:48 -0500

    post feed functional with timestamps

--- components/Postfeed.js
+++ components/Postfeed.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import Tweet from "./Tweet";
 import TweetInput from "./TweetInput";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
 
 export default function PostFeed() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setTweets(snapshot.docs);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <div
       className="sm:ml-16 xl:ml-80 max-w-2xl flex-grow
@@ -16,6 +29,9 @@ export default function PostFeed() {
         Home
       </div>
       <TweetInput />
+      {tweets.map((tweet) => {
+        return <Tweet key={tweet.id} data={tweet.data()} />;
+      })}
       <Tweet />
     </div>
   );
--- components/Tweet.js
+++ components/Tweet.js
@@ -4,11 +4,18 @@ import {
   HeartIcon,
   UploadIcon,
 } from "@heroicons/react/outline";
+import Moment from "react-moment";
 
-export default function Tweet() {
+export default function Tweet({ data }) {
   return (
     <div className="border-b border-gray-700">
-      <TweetHeader />
+      <TweetHeader
+        username={data?.username}
+        name={data?.name}
+        timestamp={data?.timestamp?.toDate()}
+        text={data?.tweet}
+        photoUrl={data?.photoUrl}
+      />
       <div className="p-3 ml-16 text-gray-500 flex space-x-14">
         <ChatIcon className="w-5 cursor-pointer hover:text-green-400" />
         <HeartIcon className="w-5 cursor-pointer hover:text-pink-500" />
@@ -19,20 +26,18 @@ export default function Tweet() {
   );
 }
 
-export function TweetHeader() {
+export function TweetHeader({ username, name, timestamp, text, photoUrl }) {
   return (
     <div className="flex space-x-3 p-3 border-gray-700">
-      <img
-        className="w-11 h-11 rounded-full object-cover"
-        src="/assets/kylie.png"
-      />
+      <img className="w-11 h-11 rounded-full object-cover" src={photoUrl} />
       <div>
         <div className=" text-gray-500 flex items-center space-x-2 mb-1">
-          <span>@kylie</span>
+          <h1 className="text-white font-bold">{name}</h1>
+          <span>@{username}</span>
           <div className="w-1 h-1 bg-gray-500 rounded-full "></div>
-          <span>2 hours ago</span>
+          <Moment fromNow>{timestamp}</Moment>
         </div>
-        <span>Text</span>
+        <span>{text}</span>
       </div>
     </div>
   );
