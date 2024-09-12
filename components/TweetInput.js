2b84c5a18610fdbf773f465686d88dcd4f8fd365
brandonbuhr <brandonbuhr35@gmail.com>
2024-09-11 19:02:29 -0500

    added placeholder image

--- components/TweetInput.js
+++ components/TweetInput.js
@@ -76,7 +76,7 @@ export default function TweetInput() {
     <div className="flex space-x-3 p-3 border-b border-gray-700">
       <img
         className="w-11 h-11 rounded-full object-cover"
-        src={user.photoUrl || "/assets/twitter-brands-solid.svg"}
+        src={user.photoUrl || "/assets/placeholder.png"}
       />
 
       {loading && <h1 className="text-2xl text-gray-500">Uploading post...</h1>}
